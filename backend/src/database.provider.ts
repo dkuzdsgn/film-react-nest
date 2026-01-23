f963745fc91cfb4c0830797497aa4ae2fb95dc1d
Daria Kuznetsova <dkuzdsgn@gmail.com>
2026-01-23 13:17:02 +0400

    fixing

--- backend/src/database.provider.ts
+++ backend/src/database.provider.ts
@@ -1,10 +1,6 @@
-import { FilmsMongoRepository } from './repository/film.mongo.repository';
 import { FilmsTypeormRepository } from './repository/film.typeorm.repository';
 
 export const databaseProvider = {
   provide: 'FilmsRepository',
-  useClass:
-    process.env.DATABASE_DRIVER === 'postgres'
-      ? FilmsTypeormRepository
-      : FilmsMongoRepository,
+  useClass: FilmsTypeormRepository,
 };
